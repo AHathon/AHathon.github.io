f3762647c36a08badf01ff6c20884fd00c855f0e
AHathon <a_hathon@yahoo.com>
2024-06-21 18:12:56 -0400

    minor fixes

--- src/background.js
+++ src/background.js
@@ -11,11 +11,22 @@ document.addEventListener("DOMContentLoaded", function(e) {
    
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement )
+    document.body.appendChild( renderer.domElement );
     
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 100, 500 );
   
+    // responsive resizing handler
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+    window.addEventListener("resize", onWindowResize, false);
+
+    // 
+    
     let vCheck = false;
 
     camera.position.z = 5;
